17215dc81bc772aebd4b90101b6f48b3063a29cd
Tewis LEMAIRE <tewis.lemaire@laposte.net>
2019-11-28 15:50:18 +0100

    Argument XML

--- pdftotext.py
+++ pdftotext.py
@@ -1,8 +1,9 @@
-import shutil   #pour suprimmer récursivement
+import shutil   #pour suprimmer recursivement
 import subprocess as sp #commande 
 import os       #commande de base
 import os.path  #le path du program
 import sys      #argument 
+from bs4 import BeautifulSoup
 
 def transformEintoE(element):
     cpt = 0
@@ -16,7 +17,7 @@ def transformEintoE(element):
 
 def filtre(src,dst):
     for ligne in src:
-        print(ligne)
+        print(ligne, file=dst)
         
         
 def pdf(arg):
@@ -53,22 +54,57 @@ def transmog(arg):
             #filtre(source,destination)
             source.close()
             destination.close()
+
+
+def transmogXML(arg):
+    result = "{0}/{1}result".format(os.getcwd(), arg)
+    if os.path.exists(result):
+        shutil.rmtree(result)
+    os.mkdir(result)
+    tmp = "{0}/{1}/tmp".format(os.getcwd(), arg)
+    for element in os.listdir(tmp):
+        if element.endswith('.xml'):
+            a = "../result/"
+            os.chdir(tmp)
+            source = open(element,"r")
+            destination = open(a+element, "w")
+            source.close()
+            destination.close()   
             
             
             
             
-if len(sys.argv) != 2:
-    print("Un seul argument attendu !")
+if len(sys.argv) > 3:
+    print("Deux arguments attendu au maximum !")
     print(sys.argv)
     sys.exit(2)
-else:
+if len(sys.argv) == 1:
     current = os.getcwd()
     if os.path.exists(sys.argv[1]) & os.path.isdir(sys.argv[1]):
-       # os.chdir(sys.argv[1])
-        pdf(sys.argv[1])
-        transmog(sys.argv[1])
-       # sp.Popen("rm tmp")
-    else:
-        #print(sys.argv[1])
-        print( "L'argument n'éxiste pas ou n'est pas un répertoire !")
+        pdf(sys.argv[2])
+        transmog(sys.argv[2])
         sys.exit(2)
+    else:
+        print( "L'argument n'existe pas ou n'est pas un repertoire !")
+        sys.exit(2)
+if len(sys.argv) == 2 :
+    if sys.argv[1] == "-t":
+        if os.path.exists(sys.argv[1]) & os.path.isdir(sys.argv[1]):
+            pdf(sys.argv[2])
+            transmog(sys.argv[2])
+        else:
+            print( "L'argument n'existe pas ou n'est pas un repertoire !")
+            sys.exit(2)
+    elif sys.argv[1] == "-x":
+        if os.path.exists(sys.argv[1]) & os.path.isdir(sys.argv[1]):
+            pdf(sys.argv[2])
+            transmogXML(sys.argv[2])
+            sys.exit(2)
+        else:
+            print( "L'argument n'existe pas ou n'est pas un repertoire !")
+            sys.exit(2)
+    else:
+         print( "Veuillez choisir un type de sortie convenable (-x ou -t)")     
+         sys.exit(2)   
+
+#librairie beautifultree pour XML
