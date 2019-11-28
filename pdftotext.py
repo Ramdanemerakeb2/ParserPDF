3b318f695f701eaacd1707ef6aff0f9a00bedc7f
Baptise Quay <Baptise.Quay@gmail.com>
2019-11-28 15:49:24 +0100

    V1.0 avec main

--- pdftotext.py
+++ pdftotext.py
@@ -28,12 +28,7 @@ def pdf(arg):
             element = transformEintoE(element)
             titre = element[0:-4]
             a = "pdftotext -raw -nopgbrk -enc ASCII7 {0}/{1}/{2}  {0}/{1}/tmp/{3}.txt".format(os.getcwd(),arg, element, titre)
-            #print(a)
             os.system(a)
-          #  sp.run(["pdf2txt","-o" ,arg,"/tmp/",element, ".txt ", arg,element])
-        #  a = "pdf2txt -p[1] {1}/{2} > {1}/tmp/{2}.txt".format(os.getcwd(),arg, element) 
-        
-        
 
 def transmog(arg):
     origin = "{0}/{1}".format(os.getcwd(), arg)
@@ -44,7 +39,6 @@ def transmog(arg):
     tmp = "{0}/{1}/tmp".format(os.getcwd(), arg)
     for element in os.listdir(tmp):
         if element.endswith('.txt'):
-            #element = transformEintoE(element)
             os.chdir(tmp)
             source = open(element,"r")
             destination = open(result+'/'+element, "w")
@@ -52,21 +46,22 @@ def transmog(arg):
             source.close()
             destination.close()
     os.chdir(origin)
-            
-            
-            
-            
-if len(sys.argv) != 2:
-    print("Un seul argument attendu !")
-    print(sys.argv)
-    sys.exit(2)
-else:
-    current = os.getcwd()
-    if os.path.exists(sys.argv[1]) & os.path.isdir(sys.argv[1]):
-        pdf(sys.argv[1])
-        transmog(sys.argv[1])
-        #os.system("rm -r tmp")
-    else:
-        #print(sys.argv[1])
-        print( "L'argument n'éxiste pas ou n'est pas un répertoire !")
+ 
+
+def main(argv):               
+    if len(argv) != 2:
+        print("Un seul argument attendu !")
+        print(argv)
         sys.exit(2)
+    else:
+        current = os.getcwd()
+        if os.path.exists(argv[1]) & os.path.isdir(argv[1]):
+            pdf(argv[1])
+            transmog(argv[1])
+            #os.system("rm -r tmp")
+        else:
+            print( "L'argument n'éxiste pas ou n'est pas un répertoire !")
+            sys.exit(2)
+
+
+main(sys.argv)
