5c0a9f2210ef1c01a0289c4069aad6223420edfb
Merakeb <ramdane.merakeb@alumni.univ-avignon.fr>
2019-12-02 17:32:18 +0100

    Sprint2 Abstract + Nom du fichier

--- pdftotext.py
+++ pdftotext.py
@@ -1,9 +1,35 @@
-import shutil   #pour suprimmer récursivement
-import subprocess as sp #commande 
-import os       #commande de base
-import os.path  #le path du program
-import sys      #argument 
+import shutil   
+import subprocess as sp 
+import os       
+import os.path  
+import sys      
 
+def Resumé(Contenu):
+    if "Abstract" in Contenu :
+        begin = Contenu.split("Abstract",1)
+    
+    if "ABSTRACT" in Contenu :                                       
+        begin = Contenu.split("ABSTRACT",1)                     
+                       
+    end="1 "
+    
+    if "Index" in Contenu :
+        end = "Index"
+    elif "Keywords" in Contenu :                                      
+        end = "Keywords"
+            
+    
+    try:
+        begin
+    except NameError:
+        return "erreur"
+    else:
+        a = begin[1].split(end)
+        b = a[0].split("\n")
+        resumé = ''.join(b)
+        return resumé
+        
+     
 def transformEintoE(element):
     cpt = 0
     for i in element :
@@ -13,13 +39,22 @@ def transformEintoE(element):
         cpt+=1
     return element
 
-def filtre(src,dst):
-    for ligne in src:
-        print(ligne, file=dst)
+def resultat(source,destination,element):
+	nom = element[:-4] + '.pdf'
+	destination.write("nom du fichier : ")
+	print(nom, file =destination)
+	txt = source.read()
+	r = Resumé(txt)
+	destination.write("\nResumé : ")
+	for i in range(0,len(r)) :                                 
+		destination.write(r[i])
+    
+    #for ligne in src:
+        #print(ligne, file=dst)
         
         
-def pdf(arg):
-    tmp = "{}/tmp".format(arg)
+def pdftotext(arg):
+    tmp = "{}/txtTemporaire".format(arg)
     if os.path.exists(tmp):
         shutil.rmtree(tmp)
     os.mkdir(tmp)
@@ -27,22 +62,22 @@ def pdf(arg):
         if element.endswith('.pdf'):
             element = transformEintoE(element)
             titre = element[0:-4]
-            a = "pdftotext -raw -nopgbrk -enc ASCII7 {0}/{1}/{2}  {0}/{1}/tmp/{3}.txt".format(os.getcwd(),arg, element, titre)
+            a = "pdftotext -raw -nopgbrk -enc ASCII7 {0}/{1}/{2}  {0}/{1}/txtTemporaire/{3}.txt".format(os.getcwd(),arg, element, titre)
             os.system(a)
 
 def transmog(arg):
     origin = "{0}/{1}".format(os.getcwd(), arg)
-    result = "{0}/{1}result".format(os.getcwd(), arg)
+    result = "{0}/{1}resultat".format(os.getcwd(), arg)
     if os.path.exists(result):
         shutil.rmtree(result)
     os.mkdir(result)
-    tmp = "{0}/{1}/tmp".format(os.getcwd(), arg)
+    tmp = "{0}/{1}/txtTemporaire".format(os.getcwd(), arg)
     for element in os.listdir(tmp):
         if element.endswith('.txt'):
             os.chdir(tmp)
             source = open(element,"r")
             destination = open(result+'/'+element, "w")
-            filtre(source,destination)
+            resultat(source,destination,element)
             source.close()
             destination.close()
     os.chdir(origin)
@@ -56,7 +91,7 @@ def main(argv):
     else:
         current = os.getcwd()
         if os.path.exists(argv[1]) & os.path.isdir(argv[1]):
-            pdf(argv[1])
+            pdftotext(argv[1])
             transmog(argv[1])
             #os.system("rm -r tmp")
         else:
